c3bd9f65554de2366554224e963876ce8559202f
yyp <yuy20170301@126.com>
2022-06-30 16:10:08 +0800

    尝试使用模版语法

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
+const refCount = ref(0);
+const onClick = () => {
+  refCount.value += 1;
+};
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div>{{ refCount }}</div>
+  <button @click="onClick">+1</button>
 </template>
 
 <style>
