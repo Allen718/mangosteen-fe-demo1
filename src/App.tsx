d9e44197a72895746fdf30b99b5968e7a759b6b8
yyp <yuy20170301@126.com>
2022-07-01 18:35:33 +0800

    welcome 页面初始化

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,8 @@ import { RouterView } from "vue-router";
 
 export const App = defineComponent({
   setup() {
-    const refCount = ref(0);
-    const onClick = () => {
-      refCount.value += 1
-    };
     return () => <>
-      <div>
-        <div>{refCount.value}</div>
-        <button onClick={onClick}>+1</button>
         <div><RouterView /></div>
-      </div>
     </>
   }
 })
--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,17 @@
 import { Foo } from "./pages/Foo";
 import { Home } from "./pages/Home";
+import { First } from "./pages/welcome/components/First";
+import { Forth } from "./pages/welcome/components/Forth";
+import { Second } from "./pages/welcome/components/Second";
+import { Third } from "./pages/welcome/components/Third";
+import { Welcome } from "./pages/welcome/Welcome";
 
 export const routes = [
-  { path: "/", component: Home },
-  { path: "/about", component: Foo },
+  { path: "/", component: Home},
+  { path: "/welcome", component: Welcome,children:[
+    { path:'first',component:First},
+    { path:'second',component:Second},
+    { path:'third',component:Third},
+    { path:'forth',component:Forth},
+  ] },
 ];
