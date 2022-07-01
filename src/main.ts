279eb1f1babc9faf2958f038c18831bcb1379689
yyp <yuy20170301@126.com>
2022-07-01 18:12:43 +0800

    路由文化单独定义

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,8 @@
 import { createApp } from "vue";
 import { createRouter, createWebHashHistory } from "vue-router";
 import { App } from "./App";
-import { Home } from "./pages/Home";
-import { Foo } from "./pages/Foo";
+import { routes } from "./routes";
 
-const routes = [
-  { path: "/", component: Home },
-  { path: "/about", component: Foo },
-];
 const router = createRouter({
   // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
   history: createWebHashHistory(),
---
+++ src/routes.ts
@@ -0,0 +1,7 @@
+import { Foo } from "./pages/Foo";
+import { Home } from "./pages/Home";
+
+export const routes = [
+  { path: "/", component: Home },
+  { path: "/about", component: Foo },
+];
