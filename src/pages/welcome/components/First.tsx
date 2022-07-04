5ce4d8195a2ddc7ff3ac233fbd5f8863ae5e444a
yyp <yuy20170301@126.com>
2022-07-04 15:39:34 +0800

    完全仿照react写法

--- src/pages/welcome/components/First.tsx
+++ src/pages/welcome/components/First.tsx
@@ -1,22 +1,19 @@
-import { defineComponent } from 'vue';
+import { FunctionalComponent } from 'vue';
 import { RouterLink } from 'vue-router';
 import pig from '../../../assets/icons/pig.svg';
 import { WelcomeLayout } from './WelcomeLayout';
 import style from '../Welcome.module.scss';
 
-export const First = defineComponent({
-  setup: (props, context) => {
-    const slots = {
-      icon: () => <img src={pig} />,
-      title: () => <h2>会挣钱<br />还会省钱</h2>,
-      buttons: () => <>
-        <RouterLink class={style.fake} to="/start" >跳过</RouterLink>
-        <RouterLink to="/welcome/second" >下一页</RouterLink>
-        <RouterLink to="/start" >跳过</RouterLink>
-      </>
-    }
-    return () => (
-      <WelcomeLayout v-slots={slots} />
-    )
-  }
-})
+export const First: FunctionalComponent = () => {
+  return <WelcomeLayout>{{
+    icon: () => <img src={pig} />,
+    title: () => <h2>会挣钱<br />还会省钱</h2>,
+    buttons: () => <>
+      <RouterLink class={style.fake} to="/start" >跳过</RouterLink>
+      <RouterLink to="/welcome/second" >下一页</RouterLink>
+      <RouterLink to="/start" >跳过</RouterLink>
+    </>
+  }}</WelcomeLayout>
+}
+
+First.displayName = 'First'
