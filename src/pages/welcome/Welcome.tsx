09ab7d2547cc14c13ea1799101caad591ff41eee
yyp <yuy20170301@126.com>
2022-07-04 10:34:37 +0800

    解决edge浏览器页面bug

--- src/pages/welcome/Welcome.tsx
+++ src/pages/welcome/Welcome.tsx
@@ -1,25 +1,27 @@
 import { defineComponent } from 'vue';
-import { RouterView ,RouterLink} from 'vue-router';
+import { RouterView, RouterLink } from 'vue-router';
 import logo from '../../assets/icons/mangosteen.svg'
 import style from './Welcome.module.scss'
 
 export const Welcome = defineComponent({
   setup: (props, context) => {
     return () => (
-      <div class={style.wrapper}>
-        <header class={style.header}>
-          <img class={style.img} src={logo} />
-          <h class={style.text}>山竹记账</h>
-        </header>
-        <div class={style.main}>
-          <RouterView />
+      <>
+        <div class={style.wrapper}>
+          <header class={style.header}>
+            <img class={style.img} src={logo} />
+            <h class={style.text}>山竹记账</h>
+          </header>
+          <div class={style.main}>
+            <RouterView />
+          </div>
         </div>
         <footer class={style.footer}>
           <RouterLink class={style.fake} to="/start" >跳过</RouterLink>
           <RouterLink to="/welcome/2" >下一页</RouterLink>
           <RouterLink to="/start" >跳过</RouterLink>
         </footer>
-      </div>
+      </>
     )
   }
 })
