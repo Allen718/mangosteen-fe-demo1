b9790643f6c814fd09acc1df86a6d7ad3c413c1f
yyp <yuy20170301@126.com>
2022-07-06 17:04:08 +0800

    使用hook对屏幕滑动监听

--- src/pages/welcome/Welcome.tsx
+++ src/pages/welcome/Welcome.tsx
@@ -1,20 +1,29 @@
-import { defineComponent, h, Transition, VNode } from 'vue';
+import { defineComponent, h, Transition, VNode, ref, watchEffect } from 'vue';
 import { RouteLocationNormalizedLoaded, RouterView, useRoute } from 'vue-router';
-import logo from '../../assets/icons/mangosteen.svg'
+import { useSwipe } from '../../hooks/useSwipe';
 import style from './Welcome.module.scss'
 
 export const Welcome = defineComponent({
   setup: (props, context) => {
+    const main = ref<HTMLElement | null>(null);
+    const { direction, distance, swiping } = useSwipe(main);
+    watchEffect(() => {
+      if (direction.value === 'right' && distance.value) {
+        if (distance.value.x > 100) {
+          console.log('右滑动')
+        }
+      }
+    })
     return () => (
       <>
         <div class={style.wrapper}>
           <header class={style.header}>
-          <svg>
-          <use xlinkHref='#mangosteen'></use>
-        </svg>
+            <svg>
+              <use xlinkHref='#mangosteen'></use>
+            </svg>
             <h class={style.text}>山竹记账</h>
           </header>
-          <main class={style.main}>
+          <main class={style.main} ref={main}>
             <RouterView name="main">
               {({ Component: X, route: R }: { Component: VNode, route: RouteLocationNormalizedLoaded }) =>
                 <Transition enterFromClass={style.slide_fade_enter_from} enterActiveClass={style.slide_fade_enter_active}
