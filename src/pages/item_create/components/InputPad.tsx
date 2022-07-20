4b4ac90846b55823c7cc3a3373b121b407a838ad
yyp <yuy20170301@126.com>
2022-07-20 14:25:13 +0800

    feat(记账页面): inputPad 引入日期选择组件

--- src/pages/item_create/components/InputPad.tsx
+++ src/pages/item_create/components/InputPad.tsx
@@ -1,4 +1,6 @@
-import { defineComponent, PropType } from 'vue';
+import { defineComponent, PropType, ref } from 'vue';
+import { DatetimePicker, Popup } from 'vant';
+import { time } from '../../../utils/time';
 import { Icon } from '../../../components/Icon/Icon';
 import s from './InputPad.module.scss';
 export const InputPad = defineComponent({
@@ -24,20 +26,71 @@ export const InputPad = defineComponent({
       { text: '.' },
       { text: '0' },
       { text: '删除' },
-      { text: '完成' },
+      { text: '完成', },
 
     ];
+    const refShowPop = ref(false);
+    const refDate = ref();
+    const refAmount = ref('0');
+    const handleSelectDate = (value: Date) => {
+      refDate.value = value;
+      refShowPop.value = false
+    };
+    const handleAppendText = (n: string) => {
+      const dotIndex = refAmount.value.indexOf('.');
+      //限制位数
+      if (refAmount.value.length >= 13) {
+        return
+      }
+      //保留小数点后两位
+      if (dotIndex >= 0 && refAmount.value.length - dotIndex > 2) {
+        return
+      }
+      if (n === '0') {
+        if (refAmount.value === '0') {
+          return;
+        }
+      } else if (n === '.') {
+        if (dotIndex >= 0) {
+          return;
+        }
+      } else {
+        if (refAmount.value === '0') {
+          return refAmount.value = n
+        }
+      }
+
+      refAmount.value += n
+    };
     return () => (
       <>
         <div class={s.dateAndAmount}>
           <span class={s.date}>
             <Icon name="date" class={s.icon} />
-            <span>2022-01-01</span>
+            <span>
+              <span onClick={() => refShowPop.value = true}>{time(refDate.value).format('YYYY-MM-DD')}</span>
+              <Popup position='bottom' v-model:show={refShowPop.value}>
+                <DatetimePicker
+                  // v-model={refDate.value}
+                  value={refDate.value}
+                  type="date"
+                  title="选择年月日"
+                  defaultValue={refDate.value}
+                  onConfirm={handleSelectDate}
+                  onCancel={() => refShowPop.value = false}
+                />
+              </Popup>
+            </span>
           </span>
-          <span class={s.amount}>12345</span>
+          <span class={s.amount}>{refAmount.value}</span>
         </div>
         <div class={s.buttons}>
-          {buttons.map(button => <button >{button.text}</button>)}
+          {buttons.map(button => <button onClick={() => {
+            if (button.text === '清空') {
+              return refAmount.value = '0'
+            }
+            handleAppendText(button.text)
+          }}>{button.text}</button>)}
         </div>
       </>
     )
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,13 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { svgstore } from "./src/vite_plugins/svgstore";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import styleImport, { VantResolve } from "vite-plugin-style-import";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 8088,
+  },
   plugins: [
     vue(),
     vueJsx({
@@ -12,5 +16,8 @@ export default defineConfig({
       mergeProps: true,
     }),
     svgstore(),
+    styleImport({
+      resolves: [VantResolve()],
+    }),
   ],
 });
