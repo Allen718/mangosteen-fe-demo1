fbebb8c8a4628f9e9aa02428bcedc4fb151b00ab
yyp <yuy20170301@126.com>
2022-07-29 11:23:03 +0800

    feat(viteconfig): 配置别名  支持别名引入

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,18 @@ import vue from "@vitejs/plugin-vue";
 import { svgstore } from "./src/vite_plugins/svgstore";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import styleImport, { VantResolve } from "vite-plugin-style-import";
+import path from 'path'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 8088,
+  },
+  resolve: {
+    alias: {
+      '@': path.join(__dirname, './src')
+    },
   },
   plugins: [
     vue(),
@@ -21,3 +28,4 @@ export default defineConfig({
     }),
   ],
 });
+
