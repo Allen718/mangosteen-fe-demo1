b3c3d25e80e2acfccd6d8bc71ac52b92ef9d1c49
yyp <yuy20170301@126.com>
2022-07-31 17:05:24 +0800

    feat(itemList): 完成页面样式

--- src/pages/item_list/ItemList.tsx
+++ src/pages/item_list/ItemList.tsx
@@ -1,8 +1,10 @@
-import { defineComponent, PropType, ref, onUpdated } from 'vue';
+import { defineComponent, PropType, ref, onUpdated, reactive } from 'vue';
 import s from './ItemList.module.scss';
 import { MainLayout } from '../../components/Layout/MainLayout';
 import { Icon } from '../../components/Icon/Icon';
+import { Time } from '@/utils/time';
 import { Tab, Tabs } from '@/components/Tab/Tab';
+import { ItemSummary } from './components/ItemSummary';
 export const ItemList = defineComponent({
   props: {
     name: {
@@ -13,19 +15,49 @@ export const ItemList = defineComponent({
   },
   setup: (props, context) => {
     const refTab = ref('本月');
+    const time = new Time(new Date());
+    const timeList = [
+      {
+        label: '本月',
+        start: time.firstDayOfMonth(),
+        end: time.lastDayOfMonth()
+      },
+      {
+        label: '上个月',
+        start: time.add(-1, 'month').firstDayOfMonth(),
+        end: time.add(-1, 'month').lastDayOfMonth()
+      },
+      {
+        label: '今年',
+        start: time.firstDayOfYear(),
+        end: time.lastDayOfYear()
+      }
+    ]
+    const customTime = reactive({
+      start: new Time(),
+      end: new Time(),
+    });
     return () => (
       <div class={s.wrapper}><MainLayout>{{
         title: () => '山竹记账',
         icon: () => <Icon name="left" />,
-        default: () => <div>
-          <Tabs selected={refTab.value} onUpdatedTab={(label) => refTab.value = label} classPrefix="customTabs" >
-            <Tab label='本月'></Tab>
-            <Tab label="上个月"><div>上个月</div></Tab>
-            <Tab label="今年"><div>今年</div></Tab>
-            <Tab label="自定义时间"><div>自定义时间</div></Tab>
-          </Tabs>
-        </div>
+        default: () => {
+          return <div>
+            <Tabs selected={refTab.value} onUpdatedTab={(label) => refTab.value = label} classPrefix="customTabs" >
+              <Tab label="这个月">
+                <div>
+                  <ItemSummary startDate={timeList[0].start.format()} endDate={timeList[0].end.format()} />
+                </div>
+              </Tab>
+              <Tab label="上个月"><div> <ItemSummary startDate={timeList[1].start.format()} endDate={timeList[1].start.format()} /></div></Tab>
+              <Tab label="今年"><div><ItemSummary startDate={timeList[2].start.format()} endDate={timeList[2].start.format()} /></div></Tab>
+              <Tab label="自定义时间"><div><ItemSummary startDate={customTime.start.format()}
+                endDate={customTime.end.format()} /></div></Tab>
+            </Tabs>
+          </div>
+        }
       }}</MainLayout></div>
     )
   }
+  
 })
