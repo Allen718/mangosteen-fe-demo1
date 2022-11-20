1ec320ee84d77f932a472d4bce91dc98dea110f9
yyp <yuy20170301@126.com>
2022-11-20 14:00:25 +0800

    refactor(main.ts): 暂时关闭路由权限

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import { App } from "./App";
 import '@svgstore';
 import { routes } from "./routes";
-import { mePromise } from "./utils/me";
+// import { mePromise } from "./utils/me";
  
 const router = createRouter({
   // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
@@ -11,21 +11,20 @@ const router = createRouter({
   routes, // short for `routes: routes`
 });
 
-router.beforeEach(async (to, from) => {
-  if (to.path === '/' || to.path.startsWith('/welcome') || to.path.startsWith('/sign_in')
-    || to.path === '/start') {
-    return true
-  } else {
-    const path = await mePromise!.then(
-      () => true,
-      () => {
-        console.log('看看吧')
-        return '/sign_in?return_to=' + to.path
-      }
-    )
-    return path
-  }
-})
+// router.beforeEach(async (to, from) => {
+//   if (to.path === '/' || to.path.startsWith('/welcome') || to.path.startsWith('/sign_in')
+//     || to.path === '/start') {
+//     return true
+//   } else {
+//     const path = await mePromise!.then(
+//       () => true,
+//       () => {
+//         return '/sign_in?return_to=' + to.path
+//       }
+//     )
+//     return path
+//   }
+// })
 
 const app = createApp(App);
 app.use(router);
