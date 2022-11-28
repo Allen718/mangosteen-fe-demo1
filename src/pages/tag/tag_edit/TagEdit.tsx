21176da3046157a9a9a1b22338195ff2f9971152
yyp <yuy20170301@126.com>
2022-11-28 14:46:05 +0800

    feat(标签编辑): 删除标签和删除标签和记账添加事件

--- src/pages/tag/tag_edit/TagEdit.tsx
+++ src/pages/tag/tag_edit/TagEdit.tsx
@@ -2,8 +2,11 @@ import { Button } from '@/components/Button/Button';
 import { Icon } from '@/components/Icon/Icon';
 import { MainLayout } from '@/components/Layout/MainLayout';
 import { defineComponent, PropType } from 'vue';
+import { Dialog } from 'vant';
 import { TagForm } from '../tag_form/TagForm';
 import s from './TagEdit.module.scss';
+import { request } from '../../../utils/request';
+import { useRouter } from 'vue-router';
 export const TagEdit = defineComponent({
   props: {
     name: {
@@ -11,6 +14,18 @@ export const TagEdit = defineComponent({
     }
   },
   setup: (props, context) => {
+    const router=useRouter()
+    //删除标签
+    const onDelete = () => {
+      Dialog.confirm({
+        title: '确认',
+        message: '你真的要删除吗?'
+      }).then(() => {
+        // request.delete(`/tags/${router.id}`)
+        router.back()
+      })
+    };
+    //删除标签和记账
     return () => (
       <div class={s.wrapper}>
         <MainLayout>
@@ -20,8 +35,8 @@ export const TagEdit = defineComponent({
             default: () => <>
               <TagForm />
               <div class={s.actions}>
-                <Button level='danger' class={s.removeTags} onClick={() => { }}>删除标签</Button>
-                <Button level='danger' class={s.removeTagsAndItems} onClick={() => { }}>删除标签和记账</Button>
+                <Button level='danger' class={s.removeTags} onClick={onDelete}>删除标签</Button>
+                <Button level='danger' class={s.removeTagsAndItems} onClick={onDelete}>删除标签和记账</Button>
               </div>
             </>
           }}
